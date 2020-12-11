61c9e9c3cd4df35aef019b72dc9dd6b9c487baea
Tony Sims <zocle@zocle.net>
2020-12-10 21:17:24 -0600

    0-lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" check for keys to all boxes """
+
+
+def canUnlockAll(boxes):
+#!/usr/bin/python3
+""" check for keys to all boxes """
+
+
+def canUnlockAll(boxes):
+    ''' canUnlockAll '''
+
+    numboxes = len(boxes)
+    unlocked = {0}
+    keylist = [0]
+
+    if numboxes > 0:
+        while keylist:
+            nextlock = keylist.pop()
+            for nextkey in boxes[nextlock]:
+                if 0 <= nextkey <= numboxes and nextkey not in unlocked:
+                    unlocked.add(nextkey)
+                    print(unlocked)
+                    keylist.append(nextkey)
+    else:
+        return True
+
+    return numboxes == len(unlocked)
---
+++ 0x00-lockboxes/README.md
@@ -0,0 +1,2 @@
+# 0x00-lockboxes
+## 0-lockboxes.py
