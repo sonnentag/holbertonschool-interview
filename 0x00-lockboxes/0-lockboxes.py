6e963b7101748cb250748f304e3b26e2e455587e
Tony Sims <zocle@zocle.net>
2020-12-10 23:27:32 -0600

    verbose comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,17 +6,23 @@ def canUnlockAll(boxes):
     ''' canUnlockAll '''
 
     numboxes = len(boxes)
+    ''' if no boxes, go no further '''
+    if numboxes == 0:
+        return True
+
+    ''' box 0 is already unlocked '''
     unlocked = {0}
     keylist = [0]
 
-    if numboxes > 0:
-        while keylist:
-            nextlock = keylist.pop()
-            for nextkey in boxes[nextlock]:
-                if 0 <= nextkey <= numboxes and nextkey not in unlocked:
-                    unlocked.add(nextkey)
-                    keylist.append(nextkey)
-    else:
-        return True
+    ''' iterate through new boxen as they are unlocked/added '''
+    while keylist:
+        nextlock = keylist.pop()
+        for nextkey in boxes[nextlock]:
 
+            ''' add distinct new keys/boxen to keylist and unlocked set '''
+            if 0 <= nextkey <= numboxes and nextkey not in unlocked:
+                keylist.append(nextkey)
+                unlocked.add(nextkey)
+
+    ''' true if all boxes have been unlocked '''
     return numboxes == len(unlocked)
