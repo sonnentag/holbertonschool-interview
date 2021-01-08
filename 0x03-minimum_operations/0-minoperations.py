6d8ad92f0a6b03ac3c5ea3aa8d13dfc577a03960
Tony Sims <zocle@zocle.net>
2021-01-08 05:15:24 +0000

    revert to while

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,7 +9,7 @@ def minOperations(n):
 
     if n > 1:
         for i in range(2, n):
-            if (n % i == 0):
+            while (n % i == 0):
                 n = n / i
                 r += i
 
