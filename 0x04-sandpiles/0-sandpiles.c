d6a915a0aa74fd1c2f86f1e03fc5f21544455187
Tony Sims <zocle@zocle.net>
2021-01-20 11:39:17 +0000

    revised map

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,6 +1,7 @@
 #include "sandpiles.h"
 #include <stdio.h>
 
+
 /**
  * print_grid - Print 3x3 grid
  * @grid: 3x3 grid
@@ -32,16 +33,26 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 
 	int x = 0, y = 0;
+	int map = 0, pos = 1;
 
 	for (x = 0; x < 3; x++)
+	{
 		for (y = 0; y < 3; y++)
+		{
 			grid1[x][y] += grid2[x][y];
 
-	while (checkgrid(grid1))
+			if (grid1[x][y] > 3)
+				map += (pos << y);
+		}
+		pos = pos * 10;
+	}
+
+	while (map)
 	{
 		printf("=\n");
 		print_grid(grid1);
-		topple(grid1);
+		topple(grid1, map);
+		map = 0 + mapgrid(grid1);
 	}
 
 }
@@ -49,44 +60,56 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 /**
  * topple - topple grid
  * @grid: 3x3 grid
+ * @map: mapped grid of positions to topple
  *
  */
-void topple(int grid[3][3])
+void topple(int grid[3][3], int map)
 {
-	int i = 0;
-	int xf[] = {1, 0, 0, 2, 2, 0, 1, 1, 2};
-	int yf[] = {1, 0, 2, 0, 2, 1, 0, 2, 1};
+	int x, y, d, pos = 2, div = 100;
 
-	for (i = 0; i < 9; i++)
-		if (grid[xf[i]][yf[i]] >= 4)
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
 		{
-			grid[xf[i]][yf[i]] -= 4;
-
-			if (yf[i] > 0)
-				grid[xf[i]][yf[i] - 1]++;
-			if (yf[i] < 2)
-				grid[xf[i]][yf[i] + 1]++;
-			if (xf[i] > 0)
-				grid[xf[i] - 1][yf[i]]++;
-			if (xf[i] < 2)
-				grid[xf[i] + 1][yf[i]]++;
+			d = (map / div % 10) >> y;
+			d &= 1;
+			if (d)
+			{
+				grid[pos][y] -= 4;
+				if (y > 0)
+					grid[pos][y - 1]++;
+				if (y < 2)
+					grid[pos][y + 1]++;
+				if (pos > 0)
+					grid[pos - 1][y]++;
+				if (pos < 2)
+					grid[pos + 1][y]++;
+			}
 		}
+		pos -= 1;
+		div /= 10;
+	}
 }
 
 /**
- * checkgrid - test if grid is unstable
+ * mapgrid - map grid cells to topple
  * @grid: 3x3 grid
- * Return: 1 is yes, or 0
+ * Return: 0 if none to topple, or 3 digit binary repr of cells to topple
  *
  */
-int checkgrid(int grid[3][3])
+int mapgrid(int grid[3][3])
 {
 	int x = 0, y = 0;
+	int map = 0, pos = 1;
 
 	for (x = 0; x < 3; x++)
+	{
 		for (y = 0; y < 3; y++)
 			if (grid[x][y] > 3)
-				return (1);
+				map += (pos << y);
 
-	return (0);
+		pos = pos * 10;
+	}
+
+	return (map);
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void topple(int grid[3][3]);
+void topple(int grid[3][3], int map);
 int checkgrid(int grid[3][3]);
+int mapgrid(int grid[3][3]);
 
 #endif /* _SANDPILES_H */
