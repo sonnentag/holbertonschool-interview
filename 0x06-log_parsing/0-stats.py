6985ff92c628b48d05a3b68b79ce2df71ad909a1
Tony Sims <zocle@zocle.net>
2021-01-26 14:57:37 +0000

    refine style

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 """ format log stats parsed from stdin """
 
-import sys
-
-codes = {}
-count = 0
-size = 0
-
 
 def display_stats(codes, size):
     ''' function used to print formatted stats below '''
@@ -16,19 +10,26 @@ def display_stats(codes, size):
         print("{}: {}".format(k, v))
 
 
-''' expect line ending with: .. <status code> <file size>'''
-try:
-    for line in sys.stdin:
-        count += 1
-        ''' <status code> '''
-        code = line.split()[-2]
-        ''' <file size> '''
-        size += int(line.split()[-1])
-        ''' increment/add code to dict '''
-        codes[code] = codes.get(code, 0) + 1
-        if count == 10:
-            display_stats(codes, size)
-            count = 0
+if __name__ == "__main__":
+    ''' expect line ending with: .. <status code> <file size>'''
 
-except KeyboardInterrupt:
-    display_stats(codes, size)
+    import sys
+
+    codes = {}
+    count = 0
+    size = 0
+
+    try:
+        for line in sys.stdin:
+
+            count += 1
+            size += int(line.split()[-1])
+            code = line.split()[-2]
+            codes[code] = codes.get(code, 0) + 1
+
+            if count == 10:
+                display_stats(codes, size)
+                count = 0
+
+    except KeyboardInterrupt:
+        display_stats(codes, size)
