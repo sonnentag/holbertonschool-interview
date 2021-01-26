618cdb3a11884a2cc3a8fb5626a39d04a3ce1775
Tony Sims <zocle@zocle.net>
2021-01-26 13:24:19 +0000

    to test

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
+""" format log stats parsed from stdin """
 
 import sys
+
+
+def display_stats(codes, size):
+    ''' function used to print formatted stats below '''
+
+    print("File size: {}".format(size))
+    for k, v in sorted(codes.items(), key=lambda x: x[0]):
+        print("{}: {}".format(k, v))
+
 
 codes = {}
 count = 0
 size = 0
 
+""" expect lines ending with: ... HTTP/1.1" <status code> <file size>"""
 try:
     for line in sys.stdin:
         count += 1
+        ''' <status code> '''
         code = line.split()[-2]
+        ''' <file size> '''
         size += int(line.split()[-1])
+        ''' increment/add code to dict '''
         codes[code] = codes.get(code, 0) + 1
         if count == 10:
-            for k, v in sorted(codes.items(), key=lambda x: x[0]):
-                print("{}: {}".format(k, v))
-            print("File size: {}".format(size))
+            display_stats(codes, size)
             count = 0
 
 except KeyboardInterrupt:
-    for k, v in sorted(codes.items(), key=lambda x: x[0]):
-        print("{}: {}".format(k, v))
-    print("File size: {}".format(size))
+    display_stats(codes, size)
