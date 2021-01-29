2b17bd3f72f25ce97f08a18ef3448c83bc759ca1
Tony Sims <zocle@zocle.net>
2021-01-29 16:45:53 +0000

    if code in codes

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,11 +2,11 @@
 """ format log stats parsed from stdin """
 
 
-def display_stats(codes, size):
+def display_stats(status, size):
     ''' function used to print formatted stats below '''
 
     print("File size: {}".format(size))
-    for k, v in sorted(codes.items()):
+    for k, v in sorted(status.items()):
         print("{}: {}".format(k, v))
 
 
@@ -15,7 +15,8 @@ if __name__ == "__main__":
 
     import sys
 
-    codes = {}
+    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    status = {}
     count = 0
     size = 0
 
@@ -24,17 +25,19 @@ if __name__ == "__main__":
             line = sys.stdin.readline()
             if not line:
                 if count >= 1:
-                    display_stats(codes, size)
+                    display_stats(status, size)
                 break
             count += 1
-            size += int(line.split()[-1])
             code = line.split()[-2]
-            codes[code] = codes.get(code, 0) + 1
+            if code in codes:
+                status[code] = status.get(code, 0) + 1
+                size += int(line.split()[-1])
 
-            if count == 10:
-                display_stats(codes, size)
-                count = 0
+                if count == 10:
+                    display_stats(status, size)
+                    count = 0
 
         except KeyboardInterrupt:
-            display_stats(codes, size)
+            if count >= 1:
+                display_stats(status, size)
             break
