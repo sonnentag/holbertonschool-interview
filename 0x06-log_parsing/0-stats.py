53f766956c4d0ccd6558b3a2b77b214bede6afbe
Tony Sims <zocle@zocle.net>
2021-01-29 17:32:41 +0000

    isint helper

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,6 +10,15 @@ def display_stats(status, size):
         print("{}: {}".format(k, v))
 
 
+def isint(s):
+    ''' helper for try isint '''
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
 if __name__ == "__main__":
     ''' expect line ending with: .. <status code> <file size>'''
 
@@ -18,7 +27,6 @@ if __name__ == "__main__":
     status = {}
     count = 0
     size = 0
-    file_size = 0
 
     while True:
         try:
@@ -27,14 +35,15 @@ if __name__ == "__main__":
                 display_stats(status, size)
                 break
             count += 1
-            if line.split()[-1] != 'Hello':
+            if isint(line.split()[-1]):
                 code = line.split()[-2]
-                status[code] = status.get(code, 0) + 1
-                size += int(line.split()[-1])
+                if isint(code):
+                    status[code] = status.get(code, 0) + 1
+                    size += int(line.split()[-1])
 
-                if count == 10:
-                    display_stats(status, size)
-                    count = 0
+                    if count == 10:
+                        display_stats(status, size)
+                        count = 0
 
         except KeyboardInterrupt:
             if count >= 1:
