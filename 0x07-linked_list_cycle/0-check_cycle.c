b525f3ded08f85a445e06efae9c7fed3ffa702c0
Tony Sims <zocle@zocle.net>
2021-02-04 22:47:00 +0000

    initial push

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check list for cycle
+ * @list: list to check
+ * Return: 1 if true or 0
+ */
+int check_cycle(listint_t *list)
+{
+	if ((!list) || (!list->next))
+		return (0);
+
+	return (compare_nodes(list, list->next));
+}
+
+/**
+ * compare_nodes - descend into list
+ * @slow: slow moving next
+ * @fast: fast moving next
+ * Return: 1 if nodes are same or 0
+ */
+int compare_nodes(listint_t *slow, listint_t *fast)
+{
+	if (fast)
+	{
+		if (slow == fast)
+			return (1);
+
+		while (fast->next)
+			return (compare_nodes(slow->next, fast->next->next));
+	}
+
+	return (0);
+}
---
+++ 0x07-linked_list_cycle/README.md
@@ -0,0 +1,3 @@
+## 0x07-linked_list_cycle
+### lists.h
+### 0-check_cycle.c
