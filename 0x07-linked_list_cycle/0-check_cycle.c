77e700a88a9e932dfa44a093ebe35b452816bcaa
Tony Sims <zocle@zocle.net>
2021-02-05 01:10:40 +0000

    comments to clarify ternaries

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -7,6 +7,7 @@
  */
 int check_cycle(listint_t *list)
 {
+	/* make sure we were passed a list to then descend */
 	return (!list || !list->next ? 0 : compare_nodes(list, list->next));
 }
 
@@ -18,7 +19,8 @@ int check_cycle(listint_t *list)
  */
 int compare_nodes(listint_t *slow, listint_t *fast)
 {
-	if (fast && fast->next)
+	/* if we were passed a fast next node which is not at the end */
+	if (fast && fast->next)     /* return 1 if nodes match or else recurse */
 		return (slow == fast ? 1 : compare_nodes(slow->next, fast->next->next));
 
 	return (0);
