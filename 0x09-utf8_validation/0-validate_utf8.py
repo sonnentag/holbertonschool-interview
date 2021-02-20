8ce3f7c9769f5c57d7e64d89505b00ceb42184b0
Tony Sims <zocle@zocle.net>
2021-02-20 11:08:58 +0000

    additional comments

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,19 +6,26 @@ def validUTF8(data):
     ''' return true or false '''
 
     count = 0
+    ''' for each number provided '''
     for d in data:
         if not count:
+            ''' count significant bits '''
             count = sigBits(d)
+            ''' 0 is first bit in 1 byte sequence ''' 
             if count == 0:
                 continue
+            ''' only 2 to 4 are valid ''' 
             if count == 1 or count > 4:
                 return False
             count -= 1
         else:
+            ''' decrement until we reach the end ''' 
+            ''' or bytes no longer begin with 10 '''
             count -= 1
             if sigBits(d) != 1:
                 return False
 
+    ''' return True if all bits are accounted for '''
     return count == 0
 
 
@@ -26,7 +33,12 @@ def sigBits(num):
     ''' count significant bits '''
 
     count = 0
+
+    ''' range 7, 6, 5 ... '''
     for i in range(7, -1, -1):
+
+        ''' check to see if our num has a 1 '''
+        ''' at i positions to the left '''
         if num & (1 << i):
             count += 1
         else:
