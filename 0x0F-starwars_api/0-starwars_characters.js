2327e3adb63d71c1d970c8970e8ec5fae43a29c1
Tony Sims <zocle@zocle.net>
2021-04-09 04:38:37 +0000

    semistandard compliant

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,6 +2,8 @@
 /* star wars api call */
 
 const args = process.argv.slice(2);
+if (args < 1) { throw new Error('Error'); }
+
 const episode = args[0];
 const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
 const request = require('request');
@@ -9,10 +11,11 @@ const request = require('request');
 let chars = [];
 
 request(url, (err, res, body) => {
-  if (err || res.statusCode !== 200) console.log(err);
-  else chars = JSON.parse(body).characters;
+  if (err || res.statusCode !== 200) throw new Error(err);
+  chars = JSON.parse(body).characters;
   chars.forEach(function (character) {
     request(character, (err, res, body) => {
+      if (err || res.statusCode !== 200) throw new Error(err);
       console.log(JSON.parse(body).name);
     });
   });
