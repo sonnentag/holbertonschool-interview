41621be1d9bf0ccce9ab93cfa9c0cfeb3eed7af8
Tony Sims <zocle@zocle.net>
2021-04-30 04:27:22 +0000

    formatted files, haven't gotten back to it

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for value in sorted array
+ * @array: array to search
+ * @size: size of array
+ * @value: int to search for
+ * Return: index where value is located
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	return (value);
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef _SEARCH_H
+#define _SEARCH_H
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _SEARCH_H */
