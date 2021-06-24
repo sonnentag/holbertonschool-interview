483333e7a8301174769a33248e26afbdd6c0cc37
Tony Sims <zocle@zocle.net>
2021-06-24 07:11:11 +0000

    to the checker

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" making change """
+
+def makeChange(coins, total):
+	''' make change
+	'''
+	if total <= 0:
+		return 0
+
+	coins.sort(reverse=True)
+	count = 0
+	while total >= 0:
+		if total < coins[-1]:
+			break
+		for i in coins:
+			if i < total:
+				count += round(total/i)
+				total = total%i
+				if total == 0:
+					return count
+
+	return -1
