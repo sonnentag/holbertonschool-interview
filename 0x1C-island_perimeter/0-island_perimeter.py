64cc1eca011ca56437fb33f945556bc02855fc97
Tony Sims <zocle@zocle.net>
2021-07-15 03:32:52 +0000

    accounting for edges

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -12,10 +12,12 @@ def island_perimeter(grid):
     for r in range(len(grid)):
         for c in range(len(grid[r])):
             if grid[r][c] == 1:
-                ''' add left and right at end of row '''
-                if (grid[r][c + 1] == 0):
-                    p += 2
-                ''' add top and bottom at end of column '''
-                if (grid[r + 1][c] == 0):
-                    p += 2
+                ''' add 4 sides by default '''
+                p += 4
+                ''' remove left and right until end of row '''
+                if (c + 1 < len(grid[r]) and grid[r][c + 1] == 1):
+                    p -= 2
+                ''' remove top and bottom until end of column '''
+                if (r + 1 < len(grid) and grid[r + 1][c] == 1):
+                    p -= 2
     return p
