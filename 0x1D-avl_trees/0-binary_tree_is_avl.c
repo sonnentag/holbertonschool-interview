6d6d8bfd962b24932a8251936198c8be37dc07ff
Tony Sims <zocle@zocle.net>
2021-07-22 11:14:48 +0000

    to the checker

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,71 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * Return: height of tree or 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_l, h_r;
+
+	if (!tree)
+		return (0);
+
+	h_l = (tree->left) ? binary_tree_height(tree->left) + 1 : 0;
+	h_r = (tree->right) ? binary_tree_height(tree->right) + 1 : 0;
+
+	return ((h_l >= h_r) ? h_l : h_r);
+}
+
+/**
+ * binary_tree_is_bst - check if avl tree is bst
+ * @tree: pointer to root node of tree to check
+ * Return: 1 if true or else 0
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree->left != NULL && tree->left->n > tree->n)
+		return (0);
+
+	if (tree->right != NULL && tree->right->n < tree->n)
+		return (0);
+
+	if (tree->left)
+		if (binary_tree_is_bst(tree->left) == 0)
+			return (0);
+
+	if (tree->right)
+		if (binary_tree_is_bst(tree->right) == 0)
+			return (0);
+
+	return (1);
+}
+
+/**
+ * binary_tree_is_avl - find the balance of an AVL tree
+ * @tree: pointer to the tree
+ * Return: 1 if true or else 0
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int balance = 0;
+
+	if (!tree)
+		return (0);
+
+	if ((!tree->left) && (!tree->right))
+		return (1);
+
+	if (binary_tree_is_bst(tree) == 0)
+		return (0);
+
+	if (tree->left)
+		balance += binary_tree_height(tree->left);
+
+	if (tree->right)
+		balance -= binary_tree_height(tree->right);
+
+	return (balance == 0 ? 1 : 0);
+}
