062549335c15c75c645632de0af925ac0dd7b4e8
Tony Sims <zocle@zocle.net>
2021-07-29 15:09:05 +0000

    removing while loop

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -15,11 +15,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-	{
-		while (*s2 + 1 == '"')
-			s2 = s2 + 1;
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
 
 	return (0);
 }
