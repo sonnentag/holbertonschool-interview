675bb9dfa3a6bb9d95e8708206cf643a8d1bb455
Tony Sims <zocle@zocle.net>
2021-07-29 14:55:07 +0000

    revised logic

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -15,7 +15,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-		return (wildcmp(s2 + 1, s1) || wildcmp(s2, s1 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
 	return (0);
 }
