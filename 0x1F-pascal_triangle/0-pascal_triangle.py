d4adc2b346a689a634d01aa4b8d95fb9a65561e2
Tony Sims <zocle@zocle.net>
2021-08-05 21:43:16 +0000

    to check

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 0x1F-pascal_triangle
+"""
+
+def pascal_triangle(n):
+	''' generate pascal's triangle of n levels deep
+	'''
+	triangle = []
+	if n <= 0:
+		return triangle
+
+	for x in range(1, n + 1):
+		row = []
+		col = 1
+		if triangle:
+			for k in range(1, x + 1):
+				row.append(col)
+				col = int(col * (x - k) / k)
+		triangle.append(row)
+	return triangle
