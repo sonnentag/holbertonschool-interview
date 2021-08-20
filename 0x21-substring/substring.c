07006772a1e222d22f4fa6a4fd3f112ff9662dec
Tony Sims <zocle@zocle.net>
2021-08-20 03:48:10 +0000

    betty

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -2,13 +2,12 @@
 
 
 /**
- * find_substring - finds all the possible substrings containing a list
- * of words, within a given string.
- * @s: string to check
- * @words: list of words
- * @nb_words: size of @words
- * @n: size of result array
- * Return: list of indecies where permutations start
+ * find_substring - finds all substrings containing a list of words
+ * @s: string to scan
+ * @words: array of words
+ * @nb_words: number of elements
+ * @n: number of elements in the returned array
+ * Return: array of indices where words were found
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
