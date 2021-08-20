a07545a9b0cd51a860ea4e3670555bfd1b76ba1e
Tony Sims <zocle@zocle.net>
2021-08-20 03:31:27 +0000

    return null

---
+++ 0x21-substring/README.md
@@ -0,0 +1,2 @@
+## 0x21-substring
+a function that finds all the possible substrings containing a list of words, within a given string.
---
+++ 0x21-substring/substring.c
@@ -0,0 +1,17 @@
+#include "substring.h"
+
+
+/**
+ * find_substring - finds all the possible substrings containing a list
+ * of words, within a given string.
+ * @s: string to check
+ * @words: list of words
+ * @nb_words: size of @words
+ * @n: size of result array
+ * Return: list of indecies where permutations start
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	if (!s || !words || !*words || !nb_words || !n)
+		return (NULL);
+}
