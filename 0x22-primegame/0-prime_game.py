d2d2f3159453a9c879cb1f8f205be504f53c4cae
Tony Sims <zocle@zocle.net>
2021-08-26 16:19:11 +0000

    first

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Prime Game '''
+
+
+def isWinner(x, nums):
+    """ See who is winner of the game
+    """
+
+    if x < 1 or not nums:
+        return None
+
+    return Ben
